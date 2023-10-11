99069a9c63407c4e27c547f75edd9378afec7890
kutlo2019 <kutlopopo@rocketmail.com>
2023-10-11 17:17:36 +0200

    add isAttacked and shipID props

--- src/Cell.js
+++ src/Cell.js
@@ -2,6 +2,8 @@ export default class Cell {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.hasShip = false
+    this.hasShip = false;
+    this.isAttacked = false;
+    this.shipID = undefined;
   }
 }
